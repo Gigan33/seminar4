2dbefbb919edc97e065e81b7606b7f33e1bf2bfe
Gigan33 <119124978+Gigan33@users.noreply.github.com>
2023-05-01 19:03:33 +0300

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,15 +1,11 @@
 #include <locale.h>
-#include "TransToDec.c"
-#include "DecToBin.c"
-
-
-
-
+#include "func.h"
 
 int mainin() {
     setlocale(LC_ALL, ".1251");
     int len1,dv;
     dv = TransToDec(len1);
     DecToBin(dv);
+
 
 }
